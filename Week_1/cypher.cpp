658a4b55bd80745f265ced91735ecc100bd69cbb
Ahnaf Anan <aanana0171@gmail.com>
2025-04-06 23:13:26 +0600

    Cypher problem solved

--- Week_1/cypher.cpp
+++ Week_1/cypher.cpp
@@ -3,72 +3,68 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
+    int tc;
+    cin >> tc;
 
-    for (int i = 0; i < n; i++)
+    while(tc--)
     {
-        cin >> arr[i];
-    }
+        int n;
+        cin >> n;
+        int arr[n];
 
-    // int w1,w2,w3;
-    // string s1,s2,s3;
-
-    // cin >> w1 >> s1;
-    // cin >> w2 >> s2;
-    // cin >> w3 >> s3;
-    
-    vector<int> w(n);
-    vector<string> s(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> w[i];
-        cin >> s[i];
-    }
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << w[i] << " ";
-    //     cout << s[i] << endl;
-    // }
-
-    vector<int> ans;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < w[i]; i++)
+        for (int i = 0; i < n; i++)
         {
-            int c = s[i][j];
-            int n;
-
-            if(arr[i] == 9 && c == 'D')
-            {
-                n = 0;
-                ans.push_back(n);
-            }
-            else if(arr[i] == 0 && c == 'U')
-            {
-                n = 9;
-                ans.push_back(n);
-            }
-            else if(c == 'D')
-            {
-                n++;
-                ans.push_back(n);
-            }
-            else if(c == 'U')
-            {
-                n--;
-                ans.push_back(n);
-            }
+            cin >> arr[i];
         }
-    }
+        
+        vector<int> w(n);
+        vector<string> s(n);
 
-    for(int x : ans)
-    {
-        cout << x << " ";
+        for (int i = 0; i < n; i++)
+        {
+            cin >> w[i];
+            cin >> s[i];
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            int elem=arr[i];
+
+            for (int j = 0; j < w[i]; j++)
+            {
+                char c = s[i][j];
+
+                if(c == 'D')
+                {
+                    if(elem == 9)
+                    {
+                        elem = 0;
+                    }
+                    else
+                    {
+                        elem++;
+                    }
+                }else if(c== 'U')
+                {
+                    if(elem == 0)
+                    {
+                        elem = 9;
+                    }
+                    else
+                    {
+                        elem--;
+                    }
+                }
+            }
+            ans.push_back(elem);
+        }
+
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
     }
     
     return 0;
