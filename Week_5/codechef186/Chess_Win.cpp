127835f9dfffba2a2914ed45c0529e03464a6996
Ahnaf Anan <aanana0171@gmail.com>
2025-05-16 16:59:17 +0600

    Codechef 186 contest problemd

---
+++ Week_5/codechef186/Chess_Win.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    
+    int diff = abs(a-b);
+
+    cout << diff+1 << endl;
+
+
+    return 0;
+}
